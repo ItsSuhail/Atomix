518c0742b6203668dd8e969ff80aa757a1db30e0
Suhail Hasan Kg <official.aitra.tech@gmail.com>
2025-10-16 18:37:10 +0530

    parsing has been achieved through a code only the true apex understands

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Atomix')
 
+text_font = pygame.font.Font('assets/fonts/font2.ttf',60)
+background_surface = pygame.image.load('assets/images/bgb.png')
+rick_surface = pygame.image.load('assets/images/rick.png')
+
+play_surface = text_font.render('PLAY', False, 'Orange')
+
+
 clock = pygame.time.Clock()
 
 while (True):
@@ -13,6 +20,9 @@ while (True):
             pygame.quit()
             exit()
     
+    screen.blit(background_surface, (0,0))
+    screen.blit(rick_surface, (0,0))
+    screen.blit(play_surface, (600,200))
 
     pygame.display.update()
     clock.tick(60) #Sets maximum frame rate (ceiling)
